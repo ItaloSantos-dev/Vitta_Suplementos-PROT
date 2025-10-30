15d6ac53c1dded97fec334697fd0e9798ff2a77b
Italo Santos <italosantoscont@gmail.com>
2025-10-30 16:35:28 -0300

    Página principal
    Criação da rota, model e vioew para listagem de produtos da página principal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,9 +2,42 @@
 @section('title', 'Pagina principal')
 
 @section('style')
-
+<link rel="stylesheet" href="{{asset('css/index.css')}}">
 @endsection
 
 @section('conteudo')
+    <main class="container mt-2 mb-2">
+        <h1 class="alert alert-success text-center">Produtos em estoque</h1>
+        <div class="row g-4">
+            @foreach($produtos as $produto)
+                <div class="col-6 col-sm-4 col-md-3 col-lg-2 d-flex gap-2">
+                    <a class="verMais" href="">
+                        <div class="card produtoCard shadow-sm rounded-3 w-100 d-flex flex-column h-100">
+                        <img src="{{$produto->imagem}}" alt="{{$produto->nome}}" 
+                            class="card-img-top p-3"
+                            style="height: 200px; object-fit: contain;">
+
+                        <div class="card-body d-flex flex-column text-center">
+                            <div class="nomeProduto ">
+                                <p class="card-title fw-semibold">{{$produto->nome}}</p>
+                            </div>
+                            <div class="precoProduto gap-0 flex-nowrap">
+                                <p class="apartir mb-0 flex-nowrap">A partir de:</p>
+                                <p class="precoVista mb-0">R$ {{$produto->preco}}</p>
+                                <p class="mb-0">ou</p>
+                                <p class="precoParcelado mb-0">Em 2X de R$ {{number_format(($produto->preco)/2, 2, ',', '.')}} sem juros</p>
+                            </div>
+                        </div>
+                        <div class="card-footer p-0 ">
+                            <form action="">
+                                <button class="btnaddCarrinho btn-sm btn btn-success">Adicionar ao carrinho</button>
+                            </form>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    </main>
 
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,58 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <style>
-        
-        body{
-            font-family: 'Montserrat', sans-serif;
-        }
-        header{
-            background-color: white;
-        }
-        #infoheader{
-            background-color: #A5D6A7;
-        }
-        .btnMenu{
-            color: rgb(0, 0, 0);
-            list-style: none;
-            background-color: #4CAF50;
-            padding: 5px;
-            transition-duration: .2s ;
-            border-radius: var(--bs-border-radius) !important;
-            box-shadow: var(--bs-box-shadow) !important;
-
-        }
-        .animar:hover {
-            background-color: #77e37b!important;
-
-        }
-        #login{
-            background-color:#4CAF50 ;
-            position: absolute;
-            left: 86vw;
-
-        }
-        #carrinho{
-            background-color:#4CAF50 ;
-            position: absolute;
-            left: 90vw;
-
-        }
-        footer{
-            background-color: #113913;
-            color: white;
-        }
-        .linksfooter{
-            color: white;
-            text-decoration: none;
-        }
-        #filtros{
-            height: auto;
-            width: 250px;
-            position: fixed;
-            top: 40vh;
-        }
-    </style>
+    <link rel="stylesheet" href="{{asset('css/layout.css')}}">
     @yield('style')
 </head>
 <body>
@@ -68,6 +17,8 @@
         <div id="infoheader" class="text-center">
             <p class="fw-bold">Tudo para sua nutrição e saúde</p>
         </div>
+
+
         <div class="logo text-center">
             <a href="#"><img src="{{asset('imagens/layout/logo.png')}}" alt="logo" class="img-fluid" style="height: 100px;"></a>
         </div>
@@ -91,42 +42,48 @@
                 <li ><a href="#" class="btnMenu text-decoration-none animar">Wheys</a></li>
                 <li ><a href="#" class="btnMenu text-decoration-none animar">Hipercalóricos</a></li>
                 <li ><a href="#" class="btnMenu text-decoration-none animar">Creatinas</a></li>
+                <li><i id="btnVerFiltros" onclick="verfiltros()" class="animar btnMenu btnVerFiltros bi bi-arrow-down">Filtrar</i>
+                    <div id="filtros" class=" m-2 text-center " >
+                        <h6 class="fw-bold">Categorias</h6>
+                        <ul class="list-unstyled">
+                            <li>Coqueteleiras</li>
+                            <li>Pre treinos</li>
+                            <li>Emagrecedores</li>
+                            <li>Multivitaminas</li>
+                        </ul>
+        
+                        <form action="" class="container p-1">
+                            <h6>Filtrar por preço</h6>
+                            <div class="row">
+                                <div class="col">
+                                    <label for="de">DE</label>
+                                    <input class=" form-control" type="number" step="0.01" name="de" id="de">
+                                </div>
+                                <div class="col">
+                                    <label for="ate">ATÉ</label>
+                                    <input class="form-control" type="number" step="0.01" name="ate" id="ate">
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col">
+                                    <input class="form-control" type="submit" value="Filtrar">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </li>
             </ul>
         </nav>
     </header>
 
 
-    <div id="filtros" class="border m-2 text-center " >
-        <h6 class="fw-bold">Categorias</h6>
-        <ul class="list-unstyled">
-            <li>Coqueteleiras</li>
-            <li>Pre treinos</li>
-            <li>Emagrecedores</li>
-            <li>Multivitaminas</li>
-        </ul>
-        <h6>Filtrar por preço</h6>
-        <form action="" class="container">
-            <div class="row">
-                <div class="col">
-                    <label for="de">DE</label>
-                    <input class=" form-control" type="number" step="0.01" name="de" id="de">
-                </div>
-                <div class="col">
-                    <label for="ate">ATÉ</label>
-                    <input class="form-control" type="number" step="0.01" name="ate" id="ate">
-                </div>
-            </div>
-            <div class="row mt-2">
-                <div class="col">
-                    <input class="form-control" type="submit" value="Filtrar">
-                </div>
-            </div>
-        </form>
-    </div>
+
+    <!-- Filtros laterais -->
+    
     @yield('conteudo')
 
 
-    <footer class=" fixed-bottom">
+    <footer class=" mt-auto">
         <div class="container">
             <div class="row">
                 <div class="col text-center">
@@ -142,5 +99,21 @@
             </div>
         </div>
     </footer>
+    <script>
+        function verfiltros(){
+            let btnverfiltros = document.getElementById('btnVerFiltros');
+            let filtros = document.getElementById('filtros')
+            if(btnverfiltros.classList.contains('bi-arrow-down')){
+                btnverfiltros.classList.replace('bi-arrow-down', 'bi-arrow-up')
+                filtros.classList.add('border', 'shadow', 'm-0')
+                filtros.style.maxHeight=filtros.scrollHeight + "px";
+            }
+            else{
+                btnverfiltros.classList.replace('bi-arrow-up', 'bi-arrow-down')
+                filtros.style.maxHeight=0;
+                filtros.classList.remove('border', 'shadow', 'm-0')
+            }
+        }
+    </script>
 </body>
 </html>
